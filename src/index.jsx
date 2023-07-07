9825fc32c31033ca7dbc3f5c46452e97ef6311c9
Rodrigo Núñez <hi@iamrodrek.com>
2023-07-07 16:29:12 +0200

    ok

--- src/index.jsx
+++ src/index.jsx
@@ -26,10 +26,10 @@ const App = () => {
           <p>
             Welcome to our tribute website, celebrating the profound impact and timeless
             work of a musician who truly left their mark on music history. Here, we delve
-            into their career highlights, explore their influential songs, and share
-            personal stories that shaped their legacy. This site is a curated journey into
-            their life and work, meant to inspire new and seasoned fans alike. Let's
-            embark on this musical adventure together.
+            into their career, explore their influential songs, and share personal stories
+            that shaped their legacy. This site is a curated journey into their life and
+            work, meant to inspire new and seasoned fans alike. Let's embark on this
+            musical adventure together!.
           </p>
           <button onClick={handleStart}>Explore</button>
         </div>
