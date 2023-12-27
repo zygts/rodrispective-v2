e1701bf410e8dd79dbcffb2aec73b6f1bbddce80
Rodrigo Núñez <hi@iamrodrek.com>
2023-12-27 14:37:49 +0100

    más música

--- src/SongCollection.jsx
+++ src/SongCollection.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState, useContext } from "react";
 import { TextureLoader } from "three";
-import { Html } from "@react-three/drei";
 
 import { AppContext } from "./appContext";
 import Cube from "./Song";
@@ -56,7 +55,7 @@ export default function CubeGroup({
         paragraph:
           "Inspired by late-night coding sessions during my early years as a programmer, 'Digital Dreams' is a vibrant journey through a pulsing cybernetic dreamscape. Synthesized melodies, reminiscent of the twinkling constellations I would gaze at from my rooftop, intertwine with futuristic beats and echo the rhythmic typing that fueled my creative process. This hypnotic dance floor anthem is a testament to those evenings that blurred the lines between reality and imagination, serving as a metaphor for my journey into the world of electronic music production.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/amanecer.mp3",
       },
       {
         title: "Things to say on a wednesday morning",
@@ -66,7 +65,7 @@ export default function CubeGroup({
         paragraph:
           " As a child, I was fascinated by the radio's ability to transport stories and emotions across vast distances. 'Frequency Fables' reflects this early fascination. This track playfully explores electronic soundscapes, each fluctuating wavelength and layered harmonic intended to transport listeners on a sonic rollercoaster ride. Every beat, bass line, and melodic rift contributes to a rich, ever-evolving narrative that tells the story of my personal journey through the world of music.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/wednesday-morning.mp3",
       },
       {
         title: "Mentiras para ilusos",
@@ -76,7 +75,7 @@ export default function CubeGroup({
         paragraph:
           " Inspired by my long-standing fascination with quantum mechanics and the nature of the universe, 'Quantum Echoes' combines elements of downtempo and ambient music to create an otherworldly atmosphere. Ethereal synths and minimalist beats echo across a universe of sound, suggesting the interconnectedness of all things and the vastness of the cosmos. It's a sonic manifestation of my contemplative moments spent stargazing, pondering the mysteries of the universe.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/mentiras.mp3",
       },
       {
         title: "Homenaje",
@@ -86,7 +85,7 @@ export default function CubeGroup({
         paragraph:
           "From a young age, I was captivated by the power of the human voice. 'Vox Illumination' is a testament to that fascination, celebrating the art of vocal manipulation. Processed vocals, transformed into transcendent melodies, weave through a labyrinth of deep bass lines and intricate beats. The resulting sonic texture is a tribute to my early choir days, the layering of human voices, and the complex harmonies we would create.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/homenaje.mp3",
       },
       {
         title: "Boca, Voz, Grito",
@@ -96,7 +95,7 @@ export default function CubeGroup({
         paragraph:
           "Growing up in a city renowned for its extensive underground network, I found a unique rhythmic pulse beneath the surface. 'Subterranean Pulse' is a sonic exploration into the heart of the earth, inspired by the subways' rhythm, the subterranean ecosystems, and the city's heartbeat. The deep, resonant bass represents this thrum, while higher synth patterns mimic life stirring beneath the urban surface.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/boca-voz-grito.mp3",
       },
       {
         title: "Farewell to None",
@@ -106,7 +105,7 @@ export default function CubeGroup({
         paragraph:
           "As a child, I was captivated by the colors of the northern lights dancing across the night sky. 'Neon Nebula' is a dynamic burst of color and sound that captures the vibrant energy of those lights. This track pulses with lively synth and progressive beats, merging the awe-inspiring beauty of a cosmic light show with the captivating energy of a supernova.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/farewell.mp3",
       },
       {
         title: "Piggy Bites",
@@ -116,7 +115,7 @@ export default function CubeGroup({
         paragraph:
           " Growing up in a bustling metropolis, I found solace in quiet parks nestled amidst the urban chaos. 'Synthetic Serenity' captures this sense of peace amid turmoil. Soft, undulating synth chords and gentle, rhythmic percussion lull the listener into a state of calm, while ambient sounds provide an organic touch to the synthesized composition, mirroring my experiences of finding tranquility in nature amidst the city's cacophony.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/piggy.mp3",
       },
       {
         title: "Airports",
@@ -126,7 +125,7 @@ export default function CubeGroup({
         paragraph:
           " Growing up in a bustling metropolis, I found solace in quiet parks nestled amidst the urban chaos. 'Synthetic Serenity' captures this sense of peace amid turmoil. Soft, undulating synth chords and gentle, rhythmic percussion lull the listener into a state of calm, while ambient sounds provide an organic touch to the synthesized composition, mirroring my experiences of finding tranquility in nature amidst the city's cacophony.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/airports.mp3",
       },
       {
         title: "Black Ghosts of The Archway",
@@ -136,7 +135,7 @@ export default function CubeGroup({
         paragraph:
           "My early music education in classical piano melded with my passion for technology in 'Cybernetic Sonata'. This track blends classical melodies with electronic rhythms, creating a fusion of old and new that captures my personal and artistic journey. The sophisticated air maintained throughout the track is a nod to my classical roots, while the heart-pounding beats reflect my immersion into the world of electronic music.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/ghosts.mp3",
       },
       {
         title: "Solo Quieren Jugar",
@@ -146,7 +145,7 @@ export default function CubeGroup({
         paragraph:
           "Inspired by my lifelong dream of space exploration, 'Astral Aura' is a musical journey through the stars. Ethereal soundscapes, celestial synths, and dreamy rhythms encapsulate the beauty of a cosmic voyage, making for a surreal and transcendent listening experience. This track embodies my curiosity and fascination with the cosmos, serving as a sonic exploration of my own 'space odyssey'.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/soloquierenjugar.mp3",
       },
       {
         title: "Un Niño",
@@ -156,7 +155,7 @@ export default function CubeGroup({
         paragraph:
           "Growing up near the ocean, I was always captivated by the rhythmic flow of the tides. 'Techno Tides' captures this natural rhythm in the context of our digital age. Thumping bass and hypnotic beats roll like waves in a digital ocean, embodying the ebb and flow of life as a modern music artist and the inevitable tides of technological change.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/nino.mp3",
       },
       {
         title: "Birthday Present",
@@ -166,7 +165,7 @@ export default function CubeGroup({
         paragraph:
           "Inspired by my urban upbringing, 'Grid Groove' pays homage to the city's grids and the vibrant life within them. With bustling beats, soaring synths, and pulsating bass, this track reflects the heart and soul of urban living, capturing the essence of city streets that formed my early experiences and influenced my music.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/birthday.mp3",
       },
       {
         title: "Requiem",
@@ -176,7 +175,7 @@ export default function CubeGroup({
         paragraph:
           "As an artist navigating the digital world, I've often marveled at our virtual interconnectedness. 'Binary Bliss' is a melodic meditation on this digital existence. The repetitive, soothing rhythms paired with floating arpeggios echo the binary code underpinning our virtual world, symbolizing my fascination with the convergence of music and technology.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/requiem.mp3",
       },
       {
         title: "Burakumin",
@@ -186,7 +185,7 @@ export default function CubeGroup({
         paragraph:
           "This high-energy track is a celebration of joy and abandon. Its infectious rhythms, catchy hook, and buoyant synths create a surge of positivity reflecting my optimistic outlook on life. 'Electro Euphoria' is about the thrill of performing live, the joy of connecting with an audience, and the shared ecstasy of a crowd moving in unison to the beat.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/burakumin.mp3",
       },
       {
         title: "He Eyes Blue Ice",
@@ -196,7 +195,7 @@ export default function CubeGroup({
         paragraph:
           "This downtempo track is a serene yet poignant ode to the day's end. Inspired by countless evenings spent coding, its warm synth pads and subtle beats evoke the feeling of watching the sun set over a cityscape. 'Silicon Sunset' captures the peace that comes with the conclusion of a productive day and the anticipation of the creative possibilities the next day holds.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/heeyes.mp3",
       },
       {
         title: "Equatorial Coordinates",
@@ -206,7 +205,7 @@ export default function CubeGroup({
         paragraph:
           "Drawing inspiration from my fascination with virtual reality, 'Harmonic Hologram' fuses lush melodies with dynamic beats to create a vivid 3D soundscape. This track immerses listeners in a multi-layered sonic experience that mirrors the multi-dimensional landscapes of VR, reflecting my ongoing exploration of the intersections between music, technology, and perception.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/equatorial.mp3",
       },
       {
         title: "Edad Sin Piedad",
@@ -216,7 +215,7 @@ export default function CubeGroup({
         paragraph:
           "Drawing inspiration from my fascination with virtual reality, 'Harmonic Hologram' fuses lush melodies with dynamic beats to create a vivid 3D soundscape. This track immerses listeners in a multi-layered sonic experience that mirrors the multi-dimensional landscapes of VR, reflecting my ongoing exploration of the intersections between music, technology, and perception.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/edad.mp3",
       },
       {
         title: "#FakeNews",
@@ -226,7 +225,7 @@ export default function CubeGroup({
         paragraph:
           "Drawing inspiration from my fascination with virtual reality, 'Harmonic Hologram' fuses lush melodies with dynamic beats to create a vivid 3D soundscape. This track immerses listeners in a multi-layered sonic experience that mirrors the multi-dimensional landscapes of VR, reflecting my ongoing exploration of the intersections between music, technology, and perception.",
         songUrl: "https://incierto.bandcamp.com/album/grito",
-        audioFileUrl: "./audio/audio.mp3",
+        audioFileUrl: "./audio/fakenews.mp3",
       },
       {
         title: "Tigre en El Ártico",
